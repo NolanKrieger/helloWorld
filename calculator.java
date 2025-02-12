265c7d6af1dc2700430e0ce09cf46824bfea392d
NolanKrieger <nolankrieger9@gmail.com>
2025-02-12 18:28:02 -0500

    added choices for multiplication and division

--- calculator.java
+++ calculator.java
@@ -8,6 +8,20 @@ public class calculator{
 
         System.out.println(first + second);
 
-     
+        System.out.println("Addition or Subtraction?");
+
+        String choice = mainScan.nextLine();
+        if(choice.equals("addition")){
+            System.out.println(first + second);
+        }else if(choice.equals("subtraction")){
+            System.out.println(first - second);
+        }
+
+        String choiceTwo = mainScan.nextLine();
+        if(choice.equals("multiplication")){
+            System.out.println(first * second);
+        }else if(choice.equals("division")){
+            System.out.println(first / second);
+        }
     }
 }
