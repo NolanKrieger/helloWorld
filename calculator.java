b7d37f1e406cf1cce97d638fa000f079d2ae8377
NolanKrieger <nolankrieger9@gmail.com>
2025-02-12 17:55:25 -0500

    added 2 numbers input and added result

---
+++ calculator.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class calculator{
+    public static void main(String[] args){
+        Scanner mainScan = new Scanner(System.in);
+
+        int first = mainScan.nextInt();
+        int second = mainScan.nextInt();
+
+        System.out.println(first + second);
+
+     
+    }
+}
